b7f36d6e9953af7079008f5153f538af9b1d6240
Artem Bulanov <aite@yandex-team.ru>
2019-10-06 21:36:04 +0300

    update build script after package change

--- buildAndLaunchMain.sh
+++ buildAndLaunchMain.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-javac src/main/java/ru/aielemental/tests/Main.java \
-      src/main/java/ru/aielemental/tests/simplegraphlib/Edge.java \
-      src/main/java/ru/aielemental/tests/simplegraphlib/DirectedEdge.java \
-      src/main/java/ru/aielemental/tests/simplegraphlib/UndirectedEdge.java \
-      src/main/java/ru/aielemental/tests/simplegraphlib/UndirectedEdge.java \
-      src/main/java/ru/aielemental/tests/simplegraphlib/Graph.java \
-      src/main/java/ru/aielemental/tests/simplegraphlib/Graphs.java
+javac src/main/java/ru/aielemental/Main.java \
+      src/main/java/ru/aielemental/simplegraphlib/Edge.java \
+      src/main/java/ru/aielemental/simplegraphlib/DirectedEdge.java \
+      src/main/java/ru/aielemental/simplegraphlib/UndirectedEdge.java \
+      src/main/java/ru/aielemental/simplegraphlib/UndirectedEdge.java \
+      src/main/java/ru/aielemental/simplegraphlib/Graph.java \
+      src/main/java/ru/aielemental/simplegraphlib/Graphs.java
 cd src/main/java
-java -Xmx64m ru.aielemental.tests.Main
+java -Xmx64m ru.aielemental.Main
